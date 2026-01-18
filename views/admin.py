ef1e4ee8e1c68e98c4c198798d91b1d9caf73bb3
Tushar Reddy <reddytushar295@outlook.com>
2026-01-18 22:10:44 +0530

    Added partial views

--- views/admin.py
+++ views/admin.py
@@ -1,3 +1,27 @@
 from __main__ import app 
 from flask import redirect,Response,url_for 
 from repository.admin import select_admin_join_user 
+
+@app.route('/admin/signup/<path:address>')
+def access_signup_page(address:str):
+    return redirect(url_for('report_exam_list')) 
+
+@app.route('/admin/users/list')
+def generate_admin_users():
+    users = select_admin_join_user()
+    print(users) 
+    user_list = [list[rec] for rec in users]
+    print(user_list)
+    content = ''' 
+                <html>
+                    <head>
+                        <title>User List</title>
+                    </head>
+                    <body>
+                        <h1>List Of Users</h1>
+                        <p>{}
+                    </body>
+                </html>
+        '''.format(user_list) 
+    res = Response(response=content,status=200,content_type='text/html') 
+    return res
---
+++ views/certificates.py
@@ -0,0 +1,45 @@
+from __main__ import app 
+from datetime import date  
+
+@app.route('/certificate/accomp/<string:name>/<string:course>/<date:accomplised_date>')
+def show_certification(name:str,course:str,accomplised_date:date):
+    cert = '''
+            <html>
+            <head>
+                <title>Certificate of Accomplishment</title>
+            </head>
+            <body>
+                <h1>Certificate of Accomplishment</h1>
+                <p>The participant {} is The participant {} is, hereby awarded this certificate of accomplishment
+                    in {} course on {} date for passing all exams. He/she proved to be ready for any of his/her future endeavors.</em>
+                </body>
+            </html>
+            '''.format(name,course,accomplised_date)
+    return cert , 200 
+
+def show_honor_dissmisal(counselor:str,effective_date:date,patient:str):
+    letter='''
+        <html>
+            <head><title>Certificate of Accomplishment</title></head>
+            <body>
+                <h1>Certificate of Accomplishment</h1>
+                    <p>From: {}
+                    <p>Head, Counselor
+                    <p>Date: {}
+                    <br/>
+                    <p>To: {}
+                    <p>Subject: Termination of consultation
+                    <br/>
+                    <p>Dear {},
+                    <p>&nbsp;&nbsp;It is with deep regret that we are informing you that we have decided to terminate you, 
+                    <p>from our institution. This is for the reason of successive absences and non-complance to the rule of consultancy. 
+                    <p>We also have to acquaint you with the reason for this decision. We wish you all the success in the future. (Cordially describe your greetings and requirements).
+                    <p>Thank you.
+                    <br/>
+
+                    <p>Yours Sincerely,
+                    <p>{}
+                </body>
+            </html>
+        '''.format(counselor,effective_date,patient,patient,counselor) 
+    return letter,200
